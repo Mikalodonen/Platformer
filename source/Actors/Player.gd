81551134a281176629921aa0df1e9931502e1a07
mikkel <mikkelandersen295@gmail.com>
2022-04-16 19:31:17 +0200

    fixing

--- source/Actors/Player.gd
+++ source/Actors/Player.gd
@@ -17,11 +17,13 @@ func _physics_process(delta: float) -> void:
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 
+
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 
 		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
 	)
+
 func calculate_move_velocity(
 		linear_velocity: Vector2,
 		direction: Vector2,
@@ -37,6 +39,7 @@ func calculate_move_velocity(
 	if is_jump_interrupted:
 			out.y = 0.0
 	return out
+
 
 func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:
 	var out: = linear_velocity
